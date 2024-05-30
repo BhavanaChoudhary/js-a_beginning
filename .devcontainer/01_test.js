a8e4409f7aaf90e5695dfc900232f2be8691d3b2
BhavanaChoudhary <135010792+BhavanaChoudhary@users.noreply.github.com>
2024-05-30 05:15:41 +0000

    !

---
+++ .devcontainer/01_test.js
@@ -0,0 +1,10 @@
+const accountId=10
+let accountEmail="abc@gmail.com"
+var accPass="12345"
+accountCity="Jaipur"
+let accState;
+accountEmail="ab@gmail.com"
+accPass="12775"
+accountCity="Blore"
+console.log(accountId);
+console.table([accountId,accountEmail,accPass,accountCity,accState])
