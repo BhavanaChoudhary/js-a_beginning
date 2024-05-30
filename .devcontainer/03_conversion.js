59720708acad73c663f38345e4e912806162b78c
BhavanaChoudhary <135010792+BhavanaChoudhary@users.noreply.github.com>
2024-05-30 06:57:54 +0000

    >>>

---
+++ .devcontainer/03_conversion.js
@@ -0,0 +1,37 @@
+// let score="33";
+// console.log(score);
+// console.log(typeof score);
+// let num=Number(score);
+// console.log(typeof num);
+// console.log(num);
+
+// let score="333a";
+// console.log(score);
+// console.log(typeof score);
+// let num=Number(score);
+// console.log(typeof num);
+// console.log(num);
+
+// let score=null;
+// console.log(typeof score);
+// console.log(score);
+
+// let score=undefined;
+// console.log(typeof score);
+// console.log(score);
+// console.log(Number(score));
+
+// let score="twenty";
+// console.log(typeof score);
+// console.log(score);
+// console.log(Number(score));
+
+
+let isloggedin=123;
+let bool=Boolean(isloggedin);
+console.log(bool);
+
+let n=123;
+let str=String(n);
+console.log(typeof str);
+console.log(str);
