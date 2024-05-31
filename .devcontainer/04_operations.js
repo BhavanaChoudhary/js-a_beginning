ae61c30d1490db98e2fa1ba9d7e9739499ac8484
BhavanaChoudhary <135010792+BhavanaChoudhary@users.noreply.github.com>
2024-05-31 02:09:55 +0000

    >>>>

---
+++ .devcontainer/04_operations.js
@@ -0,0 +1,35 @@
+// let val=3;
+// console.log(-val);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%2);
+
+// let str1="bhavana";
+// let str2=" choudhary";
+// console.log(str1+str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(+true);
+// console.log("");
+
+// let c=100;
+// // console.log(c++);
+// console.log(++c);
+
+// console.log(2>1)
+// console.log(2 >"1");
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null<0);
+// console.log(undefined>=0);
+
+console.log("2" === 2);
+console.log("2" == 2);
