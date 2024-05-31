422420086d9eef23db05f22c84ae093b3a38bf7e
BhavanaChoudhary <135010792+BhavanaChoudhary@users.noreply.github.com>
2024-05-31 17:22:18 +0000

    .

---
+++ .devcontainer/05_strings.js
@@ -0,0 +1,14 @@
+const name='bhavana';
+const rep=50;
+console.log(`my name is ${name} and repo is ${rep}`);
+const game=new String('hii  hii-all hiii');
+console.log(game[0]);
+console.log(game.__proto__);
+console.log(game.length);
+console.log(game.toUpperCase());
+console.log(game.charAt(2));
+console.log(game.indexOf('h'));
+console.log(game.slice(-6,3));
+console.log(game.trim());
+console.log(game.replace('ii','++'));
+console.log(game.split('  '));
