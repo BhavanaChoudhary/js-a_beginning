6759a78039567bac9d98b5b06095c12c029b3c7c
BhavanaChoudhary <135010792+BhavanaChoudhary@users.noreply.github.com>
2024-06-01 06:50:30 +0000

    .

--- .devcontainer/07_array.js
+++ .devcontainer/07_array.js
@@ -1,7 +1,7 @@
-// const arr=[1,2,3,4,5,true,'bhavana', a=[2,3]];
+//const arr=[1,2,3,4,5,true,'bhavana', a=[2,3]];
 // console.log(arr[7]);
-
-const a=[1,3,5,7,9];
+// arr=[0,9,8]
+//  const a=[1,3,5,7,9];
 // a.push(7)
 // console.log(a);
 // a.unshift(0);
@@ -12,10 +12,22 @@ const a=[1,3,5,7,9];
 // console.log(a.join());
 
 
-console.log("A ", a);
-const m=a.slice(1,4)
-console.log("B ", a);
-console.log(m);
-const m1=a.splice(1,4)
-console.log("C ", a);
-console.log(m1);
+// console.log("A ", a);
+// const m=a.slice(1,4)
+// console.log("B ", a);
+// console.log(m);
+// const m1=a.splice(1,4)
+// console.log("C ", a);
+// console.log(m1);
+// const d=[...arr, ...a]
+// console.log(d);
+
+
+// const n=[1,2,3,[4,5,6],6,43,[4,[5,4,0]]]
+// console.log(n.flat(Infinity));
+console.log(Array.isArray("hiiii"))
+console.log(Array.of("hiiii"))
+console.log(Array.from({name:'hiiii'}))
+
+let s1=1,s2=2,s3=3;
+console.log(Array.of(s1,s2,s3))
