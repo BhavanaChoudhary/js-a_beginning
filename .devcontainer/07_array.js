79c41bebe586ae68fed24139b29ed69a1e3b46cc
BhavanaChoudhary <135010792+BhavanaChoudhary@users.noreply.github.com>
2024-06-01 04:07:27 +0000

    .

--- .devcontainer/07_array.js
+++ .devcontainer/07_array.js
@@ -1,2 +1,21 @@
-const arr=[1,2,3,4,5,true,'bhavana', a=[2,3]];
-console.log(arr[7]);
+// const arr=[1,2,3,4,5,true,'bhavana', a=[2,3]];
+// console.log(arr[7]);
+
+const a=[1,3,5,7,9];
+// a.push(7)
+// console.log(a);
+// a.unshift(0);
+// console.log(a);
+// a.shift();
+// console.log(a);
+// console.log(a.includes(5));
+// console.log(a.join());
+
+
+console.log("A ", a);
+const m=a.slice(1,4)
+console.log("B ", a);
+console.log(m);
+const m1=a.splice(1,4)
+console.log("C ", a);
+console.log(m1);
