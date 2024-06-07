c469dfbaa9a02d703bec2be237b93f3b7ed72a00
BhavanaChoudhary <135010792+BhavanaChoudhary@users.noreply.github.com>
2024-06-07 03:23:21 +0000

    ,,

--- .devcontainer/scope.js/1_scopes.js
+++ .devcontainer/scope.js/1_scopes.js
@@ -4,3 +4,24 @@ if(true){
     console.log('inner:', a);
 }
 console.log(a);
+
+const user={
+    name:'bh',
+    p: 8,
+
+    msg: function(){
+        console.log(`hi ${this.name}`);
+    }
+}
+user.msg()
+
+// const ab =(n1,n2)=>{
+       
+//        console.log(n1+n2);
+// }
+// ab(3,4)
+const ab =(n1,n2)=>{
+       
+    return (n1+n2);
+}
+console.log(ab(3,4))
