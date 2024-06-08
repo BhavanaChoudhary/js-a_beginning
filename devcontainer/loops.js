d518bf34e276a1706026fdad3666d7c4dce7ed1d
BhavanaChoudhary <135010792+BhavanaChoudhary@users.noreply.github.com>
2024-06-08 04:29:27 +0000

    ..

---
+++ devcontainer/loops.js
@@ -0,0 +1,14 @@
+
+// for (let i = 1; i <=10; i++) {
+//     if(i==5) break;
+//     console.log(i) ;
+// }
+// for (let i = 1; i <=10; i++) {
+//     if(i==5) continue;
+//     console.log(i) ;
+// }
+let i=0
+while (i<=10) {
+    console.log('hi');
+    i++
+}
